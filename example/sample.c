a10093713f2923aa8dcb4e16306d2b2f2f413bdb
Dmitry Podgorny <pasis.ua@gmail.com>
2015-10-17 18:18:30 +0300

    Removed outdated directories

--- example/sample.c
+++
@@ -1,6 +0,0 @@
-#include "sample.h"
-
-int mod_sample(int argc, char **argv, int rd, int wr)
-{
-	return 0;
-}
--- example/sample.h
+++
@@ -1,6 +0,0 @@
-#ifndef PPPOAT_SAMPLE_H
-#define PPPOAT_SAMPLE_H
-
-int mod_sample(int argc, char **argv, int rd, int wr);
-
-#endif
